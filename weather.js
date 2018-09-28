bd3875ca5cea9e76f3809ae58ee6dba044a389f1
thadiusp <thadiusp@gmail.com>
2018-09-28 14:35:28 -0400

    Made it functional with the HTML
    I removed the getWeather function and moved the HTML posting elements in the second .then function.

--- weather.js
+++ weather.js
@@ -1,27 +1,18 @@
 const api = 'f76d4358d0078025c2a282a82459560e'
 const url = 'http://api.openweathermap.org/data/2.5/weather?q=Indianapolis&appid=';
 
-
-const getWeather = () => {
-  return fetch(url + api)
-    .then(response => response.json())
-    .then(data => {
-      const city = data.name;
-      const kTemp = data.main.temp;
-      const fTemp = Math.round((kTemp - 273.15) * 1.8 + 32);
-      const weather = data.weather[0].main;
-      const wxIcon = data.weather[0].icon;
-      console.log(`City: ${city}, Temp: ${fTemp}F, Weather: ${weather}, Weather Icon: ${wxIcon}`);
-    })
-    .catch(console.error);
-}
-
-let para = document.createElement('p')
-const weather = document.querySelector('#weather');
-weather.appendChild(para);
-
-para.textContent = `City: ${city}`;
-
-
-
-getWeather();
+fetch(url + api)
+  .then(response => response.json())
+  .then(data => {
+    const city = data.name;
+    const kTemp = data.main.temp;
+    const fTemp = Math.round((kTemp - 273.15) * 1.8 + 32);
+    const wx = data.weather[0].main;
+    const wxIcon = data.weather[0].icon;
+    let para = document.createElement('p')
+    const weather = document.querySelector('#weather');
+    weather.appendChild(para);
+    para.textContent = `City: ${city}`;
+    console.log(`City: ${city}, Temp: ${fTemp}F, Weather: ${wx}, Weather Icon: ${wxIcon}`);
+  })
+  .catch(console.error);
