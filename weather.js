cb0fec27804c937949a294c24cd7b355cc48232b
thadiusp <thadiusp@gmail.com>
2018-09-30 16:29:25 -0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 const api = 'f76d4358d0078025c2a282a82459560e'
 const url = 'http://api.openweathermap.org/data/2.5/weather?q=Indianapolis&appid=';
 
+
 fetch(url + api)
   .then(response => response.json())
   .then(data => {
@@ -8,11 +9,14 @@ fetch(url + api)
     const kTemp = data.main.temp;
     const fTemp = Math.round((kTemp - 273.15) * 1.8 + 32);
     const wx = data.weather[0].main;
-    const wxIcon = data.weather[0].icon;
-    let para = document.createElement('p')
+    const wxIcon = 'Weather Icons/' + data.weather[0].icon + '.png';
+    let para = document.createElement('p');
+    let img = document.createElement('img');
     const weather = document.querySelector('#weather');
     weather.appendChild(para);
-    para.textContent = `City: ${city}`;
+    weather.appendChild(img);
+    para.innerHTML = `City: ${city} <br> Temp: ${fTemp}F <br> Weather: ${wx}`;
+    img.src = `${wxIcon}`;
     console.log(`City: ${city}, Temp: ${fTemp}F, Weather: ${wx}, Weather Icon: ${wxIcon}`);
   })
   .catch(console.error);
